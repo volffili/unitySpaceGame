31c3078a2a66f5f667f20162d64818d149320a8f
Aleš Džubera <dzubera@uxf.cz>
2020-12-16 10:17:01 +0100

    * Added Vehicle object * Added Vehicle spawner

---
+++ SpaceGameUnity/Assets/HighCity/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    private const float CONSTANT_SPEED = 3F;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate((transform.forward) * (Time.deltaTime * CONSTANT_SPEED));
+    }
+}
---
+++ SpaceGameUnity/Assets/HighCity/VehicleSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSpawner : MonoBehaviour
+{
+    private const int SPAWN_EVERY = 2700;
+    private const int MAX_INSTANCES = 200;
+    
+    public GameObject vehiclePrefab;
+    
+    private int _spawnedInstances = 0;
+    private float _time = SPAWN_EVERY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimerTick() && _spawnedInstances < MAX_INSTANCES)
+        {
+            SpawnInstance();
+        }
+    }
+
+    private bool TimerTick()
+    {
+        _time -= Time.deltaTime * 1000;
+        if (_time <= 0)
+        {
+            _time = SPAWN_EVERY;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void SpawnInstance()
+    {
+        var vehicle = Instantiate(vehiclePrefab);
+        vehicle.transform.position = transform.position;
+        _spawnedInstances++;
+    }
+}
