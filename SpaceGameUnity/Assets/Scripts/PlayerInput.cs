f8ca357751da73ecae892abd29bd8fb8546d6e2b
Filip Volf <filip.volf@apollogames.com>
2020-12-17 13:39:23 +0100

    shitty player movement

---
+++ SpaceGameUnity/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    public InputAction movementInputVector;
+    public CharacterController controller; 
+    public float speed = 100.0f;
+
+    void Start(){
+        controller = GetComponent<CharacterController>();
+    } 
+    void OnEnable(){
+        movementInputVector.Enable();
+    }
+    void OnDisable(){
+        movementInputVector.Disable();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 inputVector = movementInputVector.ReadValue<Vector2>();
+        new Vector3(inputVector.x,0,inputVector.y);
+        controller.Move(inputVector*Time.deltaTime * speed);  
+    }
+}
